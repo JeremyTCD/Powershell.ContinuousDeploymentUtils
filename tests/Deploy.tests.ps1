62a94e8dee404c48e169c6df2fa651f6fa87df8e
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-29 21:46:51 +0800

    Added some deployment logic

--- tests/Deploy.tests.ps1
+++ tests/Deploy.tests.ps1
@@ -1,13 +1,23 @@
 $projectRoot = $PSScriptRoot | Split-Path -Parent
 $projectName = 'ContinuousDeploymentUtils'
 Invoke-Expression -Command "`"$projectRoot\src\$projectName\$projectName.pre.ps1`""
+Remove-Module $projectName
+Import-Module "$projectRoot\src\$projectName\$projectName.psd1"
 
 Describe "Deploy" {
-	BeforeAll{
-		cd $TestDrive
-	}
 	AfterAll{
+		# So test drive can be deleted
 		cd $projectRoot
+	}
+
+	BeforeEach{
+		cd $projectRoot
+		Remove-Item $TestDrive\* -Recurse -Force
+		cd $TestDrive
+
+		$env:APPVEYOR_REPO_COMMIT = 'head'
+		$env:GITHUB_TOKEN = 'test token'
+		git init 
 	}
 
 	It "Creates and pushes a tag if a version has been added to changelog" {
@@ -16,8 +26,8 @@ Describe "Deploy" {
 		$version2 = '0.2.1'
 		$version3 = '1.0.1'
 		$version4 = '1.2.1'
+		$expectedTag = "$version4+1"
 
-		git init 
 		for($i=1; $i -le 4; $i++)
 		{
 			$version = Get-Variable -Name "version$i" -ValueOnly
@@ -26,21 +36,45 @@ Describe "Deploy" {
 			git commit -m 'dummy'
 		}
 
-		#set env vars
 		$env:APPVEYOR_REPO_COMMIT = git rev-list head -1
+
+		Mock Invoke-Git -Verifiable -ParameterFilter{
+			$args[0] -eq 'push' -and `
+		    $args[1] -eq 'origin' -and `
+			$args[2] -eq $expectedTag
+		} 
 
 		# Act
 		. "$projectRoot\tools\Deploy.ps1"
 
 		# Assert
-		git tag -l | Should Be '1.2.1+1'
+		git tag -l | Should Be $expectedTag
+		Assert-VerifiableMocks
 	}
 
-	#It "Updates release if change was made to existing version's release notes" {
+	It "Creates release if new version has been added" {
+		[System.IO.File]::AppendAllText("$TestDrive\changelog.md", "`n## 0.1.1`nBody")
+		git add .
+		git commit -m 'dummy'
+
+		Mock Invoke-Git -Verifiable -ParameterFilter{
+			$args[0] -eq 'push' -and `
+			$args[1] -eq 'origin' -and `
+			$args[2] -eq '0.1.1+1'
+		}
+
+		Mock Create-GithubRelease -Verifiable -ParameterFilter{
+			$args[0] -eq $env:GITHUB_TOKEN -and `
+			$args[1] -eq '0.1.1+1' -and `
+			$args[2] -eq '0.1.1' -and `
+			$args[3] -eq "## 0.1.1`nBody" 
+		}
+
+		# Act
+		. "$projectRoot\tools\Deploy.ps1"
+	} 
+}
+
+	#It "Updates releases if version notes were changed" {
 
 	#}
-
-	#It "Creates release and publishes package if tag was pushed" {
-
-	#}
-} 
--- tools/Deploy.ps1
+++ tools/Deploy.ps1
@@ -1,46 +1,83 @@
-git config --global credential.helper store
+Invoke-Git config --global credential.helper store
 Add-Content "$($env:USERPROFILE)\.git-credentials" "https://$($env:git_key):x-oauth-basic@github.com`n"
 
-git config --global user.email $env:build_user_email
-git config --global user.name $env:build_user
+Invoke-Git config --global user.email $env:build_user_email
+Invoke-Git config --global user.name $env:build_user
 
-Write-Host "Checking if deployment is required"
-if($env:APPVEYOR_REPO_TAG){
-	Write-Host "Tag $($env:APPVEYOR_REPO_TAG_NAME) pushed ... deployment starting"
+Write-Host "Checking if changelog has changed"
+
+# check if changelog has changed
+$commit = $env:APPVEYOR_REPO_COMMIT
+$file = 'changelog.md'
+$versionHeaderPattern = '^\+##[ \t]*(\d+\.\d+\.\d+)'
+
+$changedFiles = Invoke-Git show --pretty="" --name-only $commit
+if($changedFiles -contains $file){
+	Write-Host "> Changelog changed"
+
+	$addedLines = Invoke-Git diff --unified=0 $commit^ $commit -- $file 
+	$newVersionLines = $addedLines -match $versionHeaderPattern
+	if($newVersionLines.length -eq 1){
+		$newVersionLines[0] -match $versionHeaderPattern
+		$newVersion = $matches[1]
+
+		Write-Host "> New version `"$newVersion`" detected"
+
+		$tag = "$newVersion+1"
+		Invoke-Git tag $tag
+		Invoke-Git push origin $tag -q
+
+		$env:APPVEYOR_REPO_TAG_NAME = $tag
+		Write-Host "> Tag `"$tag`" created and pushed to origin"
+	}
+	elseif($newVersionLines.Length -gt 1)
+	{
+		throw "More than one new versions detected"
+	}
+
+	Write-Host "Checking if version notes changed"
+
+	# can't run the following if commit is initial commit
+	# just don't enter this bit if there is only one version
+	# if(changelogMetadata.numVersions > 1){
+		Invoke-Git show $commit^:$file > 'changelogOld.md'
+		$addedAndRemovedFiles = Compare-TextFileLines $file 'changelogOld.md'
+		#$versionsWithChangedNotes = Get-VersionsWithChangedNotes
+	#}
 }
 else
 {
-	# check if changelog has changed
-	$commit = $env:APPVEYOR_REPO_COMMIT
-	$versionHeaderPattern = '^\+##[ \t]*(\d+\.\d+\.\d+)'
-	$changedFiles = git diff-tree --no-commit-id --name-only -r $commit
-	if($changedFiles -contains 'changelog.md'){
-		Write-Host "Changelog changed ... deployment starting"
-
-		$addedLines = git diff $commit^ $commit -- 'changelog.md' 
-		$newVersionLines = $addedLines -match $versionHeaderPattern
-		if($newVersionLines.length -eq 1){
-			$newVersionLines[0] -match $versionHeaderPattern
-			Write-Host "New version $($matches[1]) detected ... creating tag"
-
-			$tag = "$($matches[1])+1"
-			git tag $tag
-			# use gitinvoke
-			# git push origin $tag
-
-			Write-Host "Tag $tag created and pushed to origin"
-		}
-		elseif($newVersionLines.Length -gt 1)
-		{
-			Write-Error "More than one versions added"
-		}
-		else
-		{
-			# something else was changed
-		}
-	}
-	else
-	{
-		Write-Host "Deployment not required"
-	}
+	Write-Host "> Changelog unchanged..."
 }
+
+Write-Host "Checking if a new release is required"
+if($newVersion){
+	Write-Host "> New release `"$newVersion`" required"
+	$body = Get-VersionNotes $newVersion
+	Write-Host "> Release notes for `"#$newVersion`":`n$body"
+	Create-GithubRelease $env:GITHUB_TOKEN $tag $newVersion $body
+}
+else{
+	Write-Host "> New release not required"
+}
+
+#if($versionsWithChagnedNotes.length -gt 0){
+#	foreach($release in $versionsWithChagnedNotes){
+#		$body = Get-VersionNotes $release
+#		Edit-GithubRelease $env:GITHUB_TOKEN $tag $newVersion $body
+#	}
+#}
+
+# Possible that there might be a new tag without a new version if one of more
+# builds fail for a new version
+Write-Host "Checking if a tag has been added"
+if($env:APPVEYOR_REPO_TAG){
+	Write-Host "> New tag `"$($env:APPVEYOR_REPO_TAG_NAME)`" detected"
+
+	#publish
+}
+else
+{
+	Write-Host "> No new tags"
+}
+
